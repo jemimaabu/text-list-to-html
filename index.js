7a2e2116a4e3eab5ac34a9a79bec58abed6ec55b
Jemima Abu <jemimaabu@gmail.com>
2019-07-04 21:25:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Structure:
+ * Input list
+ * Use different separators for list
+ * Convert to:
+ * Html list (ordered, unordered)
+ * Html select
+ * Value options:
+ * joined by '-', joined by 'spaces', first word only
+ * Optional checkbox:
+ * Remove special characters
+ */
